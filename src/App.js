ccb4b05035da1c9d324f7abb8041f3e4be76a27e
abhinavsri360 <abhinav.022000@gmail.com>
2020-03-29 19:32:37 +0530

    before CORS

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css'
 import Home from './components/Home'
 import Department from './components/Department'
 import Employee from './components/Employee'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Navigation from './components/Navigation'
 
 function App () {
   return (
-    <div className='App'>
-      <Home />
-      <Department />
-      <Employee />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navigation />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/department' component={Department} />
+          <Route path='/employee' component={Employee} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Department.js
+++ src/components/Department.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import { Table } from 'react-bootstrap'
 
 class Department extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { deps: [] }
+  }
+
+  componentDidMount () {
+    this.refreshList()
+  }
+
+  refreshList () {
+    this.setState({
+      deps: [{ DepartmentID: 1, DepartmentName: 'IT' },
+        { DepartmentID: 2, DepartmentName: 'Biotech' }]
+    })
+  }
+
   render () {
+    const { deps } = this.state
     return (
-      <div>
-        This is Department Page.
-      </div>
+      <Table className='mt-4' striped bordered hover size='sm'>
+        <thred>
+          <tr>
+            <th>Department ID</th>
+            <th>Department Name</th>
+          </tr>
+        </thred>
+        <tbody>
+          {deps.map(dep =>
+            <tr key={dep.DepartmentID}>
+              <td>{dep.DepartmentID}</td>
+              <td>{dep.DepartmentName}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     )
   }
 }
