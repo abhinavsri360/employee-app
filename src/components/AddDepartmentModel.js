4f59d4b2c6bb11364e3225abad36cf1d35d38854
abhinavsri360 <abhinav.022000@gmail.com>
2020-04-01 00:28:22 +0530

    modals update

---
+++ src/components/AddDepartmentModel.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap'
+
+class AddDepartmentModel extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <Modal
+        {...this.props}
+        size='lg'
+        aria-labelledby='contained-modal-title-vcenter'
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id='contained-modal-title-vcenter'>
+          Add Department
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='container'>
+              
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='danger' onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
+
+export default AddDepartmentModel
--- src/components/Department.js
+++ src/components/Department.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Table } from 'react-bootstrap'
+import { Table , Button, ButtonToolbar } from 'react-bootstrap'
+import AddDepartment from './AddDepartmentModel'
 
 class Department extends Component {
   constructor (props) {
     super(props)
 
-    this.state = { deps: [] }
+    this.state = { deps: [], addModalShow: false }
   }
 
   componentDidMount () {
@@ -13,31 +14,38 @@ class Department extends Component {
   }
 
   refreshList () {
-    this.setState({
-      deps: [{ DepartmentID: 1, DepartmentName: 'IT' },
-        { DepartmentID: 2, DepartmentName: 'Biotech' }]
-    })
+    fetch('https://localhost:44325/api/Department').then(response => response.json())
+      .then(data => {
+        this.setState({ deps: data })
+      })
   }
 
   render () {
     const { deps } = this.state
+    const addModalClose = () => this.setState({ addModalShow: false })
     return (
-      <Table className='mt-4' striped bordered hover size='sm'>
-        <thred>
-          <tr>
-            <th>Department ID</th>
-            <th>Department Name</th>
-          </tr>
-        </thred>
-        <tbody>
-          {deps.map(dep =>
-            <tr key={dep.DepartmentID}>
-              <td>{dep.DepartmentID}</td>
-              <td>{dep.DepartmentName}</td>
+      <div>
+        <Table className='mt-4' striped bordered hover size='sm'>
+          <thred>
+            <tr>
+              <th>Department ID</th>
+              <th>Department Name</th>
             </tr>
-          )}
-        </tbody>
-      </Table>
+          </thred>
+          <tbody>
+            {deps.map(dep =>
+              <tr key={dep.ID}>
+                <td>{dep.ID}</td>
+                <td>{dep.DeptName}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+        <ButtonToolbar>
+          <Button variant='primary' onClick={() => this.setState({ addModalShow: true })}>Add Department</Button>
+          <AddDepartment show={this.state.addModalShow} onHide={addModalClose} />
+        </ButtonToolbar>
+      </div>
     )
   }
 }
